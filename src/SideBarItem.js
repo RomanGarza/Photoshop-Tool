d266e86963e67cd57e77275d890bab87718859ff
Roman Garza <roman_garza@protonmail.com>
2022-01-21 18:13:19 -0800

    init

--- src/SideBarItem.js
+++ src/SideBarItem.js
@@ -1,5 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function SidebarItem({ name, active, handleClick }) {
-  return
+  return (
+    <button
+      className={`sidebar-item ${active ? "active" : ""}`}
+      onClick={handleClick}
+    >
+      {name}
+    </button>
+  );
 }
